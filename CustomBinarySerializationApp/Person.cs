1c99bff25854dd09a45e73ed3d62672ec66ef607
lavetos <Hleb_Lavetka@epam.com>
2023-12-28 21:16:25 +0300

    Custom elements

---
+++ CustomBinarySerializationApp/Person.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.Serialization;
+
+
+namespace Binary
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Custom binary serialization method
+        public void SerializeCustom(FileStream stream)
+        {
+            // Convert string to bytes and write to the stream
+            byte[] nameBytes = System.Text.Encoding.UTF8.GetBytes(Name);
+            stream.Write(BitConverter.GetBytes(nameBytes.Length), 0, sizeof(int));
+            stream.Write(nameBytes, 0, nameBytes.Length);
+
+            // Write age to the stream
+            stream.Write(BitConverter.GetBytes(Age), 0, sizeof(int));
+        }
+
+        // Custom binary deserialization method
+        public static Person DeserializeCustom(FileStream stream)
+        {
+            // Read length of the name from the stream
+            byte[] lengthBytes = new byte[sizeof(int)];
+            stream.Read(lengthBytes, 0, lengthBytes.Length);
+            int nameLength = BitConverter.ToInt32(lengthBytes, 0);
+
+            // Read name bytes from the stream
+            byte[] nameBytes = new byte[nameLength];
+            stream.Read(nameBytes, 0, nameBytes.Length);
+            string name = System.Text.Encoding.UTF8.GetString(nameBytes);
+
+            // Read age from the stream
+            byte[] ageBytes = new byte[sizeof(int)];
+            stream.Read(ageBytes, 0, ageBytes.Length);
+            int age = BitConverter.ToInt32(ageBytes, 0);
+
+            return new Person { Name = name, Age = age };
+        }
+    }
+}
---
+++ CustomBinarySerializationApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create an instance of the Person class
+            Person person = new Person
+            {
+                Name = "John Doe",
+                Age = 30
+            };
+
+            // Serialize to a binary file using custom serialization
+            SerializeCustomPerson(person, "person.dat");
+
+            // Deserialize from the binary file using custom deserialization
+            Person deserializedPerson = DeserializeCustomPerson("person.dat");
+
+            // Display the deserialized person's information
+            Console.WriteLine($"Deserialized Person - Name: {deserializedPerson.Name}, Age: {deserializedPerson.Age}");
+        }
+
+        static void SerializeCustomPerson(Person person, string fileName)
+        {
+            // Use FileStream for custom binary serialization
+            using (FileStream stream = new FileStream(fileName, FileMode.Create))
+            {
+                person.SerializeCustom(stream);
+            }
+
+            Console.WriteLine("Person serialized to binary file using custom serialization.");
+        }
+
+        static Person DeserializeCustomPerson(string fileName)
+        {
+            // Use FileStream for custom binary deserialization
+            using (FileStream stream = new FileStream(fileName, FileMode.Open))
+            {
+                return Person.DeserializeCustom(stream);
+            }
+        }
+    }
+}
